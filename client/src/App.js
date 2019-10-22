c93e3bb7d8a8715470f0ca342fa4ea8ce61c9ebd
Pengwei Tian <tgun2w@gmail.com>
2019-10-22 11:35:50 -0700

    fixed App.js and working on Posts.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,27 @@
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Nav from './Components/Nav';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './Pages/home'
 import Post from './Pages/Post'
 import Posts from './Pages/Posts'
 
-function App() {
-  return (
-    <Router>
-      <div>
-        <Nav/>
+class App extends Component {
+
+
+
+  render() {
+    return (
+      <Router>
+        <Nav />
+        <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/post/" component={Post} />
-      </div>
-    </Router>
-  );
+          <Route exact path="/posts/" component={Posts} />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ client/src/Pages/Posts.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+// import axios from 'axios';
+// import API from '../Utils/API';
+import { Container } from 'react-bootstrap';
+import { CardDeck } from 'react-bootstrap';
+import { Alert } from 'react-bootstrap';
+
+class commentedPosts extends Component {
+    state ={
+        
+    }
+}
+
+export default commentedPosts;
