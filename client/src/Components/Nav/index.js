39e762dd4d4c12046a3b20a2d56aa5bf6d2cb8ae
Weston Carpenter <wc.cc96@gmail.com>
2019-10-23 10:12:39 -0700

    navbar conditionally rendered based on log state

--- client/src/Components/Nav/index.js
+++ client/src/Components/Nav/index.js
@@ -3,17 +3,35 @@ import { Nav } from 'react-bootstrap';
 import { Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 export default (props) => {
-    return (
-        <div>
-            <Navbar bg="dark" variant="dark">
-                <Link to='/'> <Navbar.Brand>Vox Populi</Navbar.Brand></Link>
-                <Nav className="mr-auto">
-                    <Nav.Link href='/login'>Login</Nav.Link>
-                    <Nav.Link href='/register'>Register</Nav.Link>
-                    <Nav.Link href='/profile'>Profile</Nav.Link>
-                    <Nav.Link href='/favorites'>Favorites</Nav.Link>
-                </Nav>
-            </Navbar>
-        </div>
-    )
+    const log = props.log;
+    console.log(log)
+    if (!log) {
+        return (
+            <div>
+                <Navbar bg="dark" variant="dark">
+                    <Link to='/'> <Navbar.Brand>Vox Populi</Navbar.Brand></Link>
+                    <Nav className="mr-auto">
+                        <Nav.Link href='/login'>Login</Nav.Link>
+                        <Nav.Link href='/register'>Register</Nav.Link>
+                        <Nav.Link href='/profile'>Profile</Nav.Link>
+                        <Nav.Link href='/favorites'>Favorites</Nav.Link>
+                    </Nav>
+                </Navbar>
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                <Navbar bg="dark" variant="dark">
+                    <Link to='/'> <Navbar.Brand>Vox Populi</Navbar.Brand></Link>
+                    <Nav className="mr-auto">
+                        <Nav.Link href='/login'>Logout</Nav.Link>
+                        <Nav.Link href='/register'>Register</Nav.Link>
+                        <Nav.Link href='/profile'>Profile</Nav.Link>
+                        <Nav.Link href='/favorites'>Favorites</Nav.Link>
+                    </Nav>
+                </Navbar>
+            </div>
+        )
+    }
 }
