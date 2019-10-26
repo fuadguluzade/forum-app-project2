ceaf479379864f5b03578a41f3d25690d3a00608
fuadguluzade <44554892+fuadguluzade@users.noreply.github.com>
2019-10-26 16:15:58 -0700

    changed mysql config

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-    host: 'y2w3wxldca8enczv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    host: 'u28rhuskh0x5paau.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     port: 3306,
-    user: 'q0vpuwzzux1zupzy',
-    password: 'xgvvrsnydjnrfhg1',
-    database: 'xekmgpypt35bjgxt'
+    user: 'aqlipr84m2saw737',
+    password: 'mj6vqcflw7xn9hql',
+    database: 'ra065ji2wgqsb8vi'
 })
 
 module.exports = connection;
